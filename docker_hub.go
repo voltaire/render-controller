3b80303a2e89a439689fe6ab2235a0828eaebc97
Jon Chen <bsd@voltaire.sh>
2020-10-11 08:27:45 +0000

    extend docker pull timeout to 1 minute

--- docker_hub.go
+++ docker_hub.go
@@ -49,7 +49,7 @@ func callbackDockerHub(w http.ResponseWriter, url string, rErr error) {
 }
 
 func (svc *server) handleDockerHubCallback(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 15*time.Second)
+	ctx, cancel := context.WithTimeout(r.Context(), time.Minute)
 	defer cancel()
 
 	var req dockerHubWebhookRequest
